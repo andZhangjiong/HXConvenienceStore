0870664a623fc4d21e4848b6ac38fd04292cc61d
andZhangjiong <207181755@qq.com>
2018-07-11 14:59:56 +0800

    双列表预览效果

---
+++ HXConvenienceStore/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HXConvenienceStore
+//
+//  Created by 张炯 on 2018/7/11.
+//  Copyright © 2018年 张炯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HXConvenienceStore/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HXConvenienceStore
+//
+//  Created by 张炯 on 2018/7/11.
+//  Copyright © 2018年 张炯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
